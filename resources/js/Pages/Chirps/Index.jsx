8a012b32c9d2b6431a99320dd2b56bd8634e5633
Bisyairi <bisyairi@farcapital.com.my>
2025-02-16 23:53:32 +0800

    first commit

---
+++ resources/js/Pages/Chirps/Index.jsx
@@ -0,0 +1,11 @@
+import Layout from '@/Layouts/GuestLayout'
+import { Head } from '@inertiajs/react'
+
+export default function Index({/*|*/}) {
+    return (
+        <Layout>
+            <Head title="Index"/>
+
+        </Layout>
+    )
+}
